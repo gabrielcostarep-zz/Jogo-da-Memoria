b0af2bc3693cee7965e12da0d2f2820b129f5e71
Gabriel Costa <gabrielcosta12377@hotmail.com>
2021-10-12 00:15:45 -0300

    codigo

---
+++ assets/scripts/script.js
@@ -0,0 +1,52 @@
+const FRONT = "card_front"
+const BACK = "card_back"
+const CARD = "card"
+const ICON = "icon"
+
+startGame()
+
+function startGame() {
+    initializeCards(game.createCardsFromTechs())
+}
+
+function initializeCards(cards) {
+    let gameBoard = document.getElementsByClassName("gameBoard")[0]
+
+    game.cards.forEach((card) => {
+        let cardElement = document.createElement('div')
+        cardElement.id = card.id
+        cardElement.classList.add(CARD)
+        cardElement.dataset.icon = card.icon
+
+        createCardContent(card, cardElement)
+
+        cardElement.addEventListener('click', flipCard)
+        gameBoard.appendChild(cardElement)
+
+    })
+}
+
+function createCardContent(card, cardElement) {
+    createCardFace(FRONT, card, cardElement)
+    createCardFace(BACK, card, cardElement)
+}
+
+function createCardFace(face, card, element) {
+    let cardElementFace = document.createElement('div')
+    cardElementFace.classList.add(face)
+
+    if (face === FRONT) {
+        let iconElement = document.createElement('img')
+        iconElement.classList.add(ICON)
+        iconElement.src = `./assets/images/${card.icon}.png`
+        cardElementFace.appendChild(iconElement)
+    } else {
+        cardElementFace.innerHTML = "&lt;/&gt;"
+    }
+
+    element.appendChild(cardElementFace)
+}
+
+function flipCard() {
+    this.classList.add("flip")
+}
