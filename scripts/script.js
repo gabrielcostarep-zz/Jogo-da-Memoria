05f01fb878d64b4ad0cb0a05318a4f006eb4fd73
Gabriel Costa <gabrielcosta12377@hotmail.com>
2021-10-12 02:31:07 -0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -48,14 +48,16 @@ function createCardFace(face, card, element) {
 }
 
 function flipCard() {
+    let firstCardView = document.getElementById(game.firstCard.id)
+    let secondCardView = document.getElementById(game.secondCard.id)
+
     if (game.setCard(this.id)) {
         this.classList.add("flip")
         if (game.checkMatch()) {
             game.clearCards()
+            this.firstCardView.classList.add("pair")
+            this.secondCardView.classList.add("pair")
         } else {
-            let firstCardView = document.getElementById(game.firstCard.id)
-            let secondCardView = document.getElementById(game.secondCard.id)
-
             setTimeout(() => {
                 firstCardView.classList.remove("flip")
                 secondCardView.classList.remove("flip")
